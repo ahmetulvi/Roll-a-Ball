ff28587afa340ca13aea41c01b6610c7455b62d1
ahmetulvi <ahmetulvi@users.noreply.github.com>
2020-12-06 22:59:33 +0300

    Initial commit

---
+++ Assets/Scenes/Kodlar/TopKontrol.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TopKontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody fizik;
+    public float hız;
+    int sayac=0;
+    public int toplanacakobjesayısı;
+    public Text sayacText;
+    public Text oyunBittiText;
+    void Start()
+    {
+         fizik = GetComponent < Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+        Vector3 vec = new Vector3(yatay, 0, dikey);
+        fizik.AddForce(vec*hız);
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "engel")
+        {
+            other.gameObject.SetActive(false);
+            sayac++;
+            sayacText.text = "Skor: " + sayac;
+            if (sayac== toplanacakobjesayısı)
+            {
+                oyunBittiText.text = "Oyun Bitti!";
+                SceneManager.LoadScene(0);
+                
+            }
+        }
+    }
+
+}
---
+++ Assets/Scenes/Kodlar/anaMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class anaMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button basla;
+    void Start()
+    {
+        Button btn = basla.GetComponent<Button>();
+        btn.onClick.AddListener(sahne);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void sahne()
+    {
+        
+        SceneManager.LoadScene(1);
+    }
+    
+}
